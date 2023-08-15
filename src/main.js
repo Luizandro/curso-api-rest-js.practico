25a1110726f406d2854bf9e1a1cc87e55fc7f9c5
Luizandro Martin <lmartin@xazar.com.mx>
2023-08-15 09:57:14 -0600

    Clase 5

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
+async function getTrendingMoviesPreview(){
+    //LLamado a la API concatenando su KEY
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    //Guardando los resultados del json
+    const data = await res.json();
+    //Del json llamo un elemento en especifico 
+    const movies = data.results;
 
-console.log('Hello');
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src',
+                              'https://image.tmdb.org/t/p/w300/' + movie.poster_path,        
+        );
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+
+
+}
+
+getTrendingMoviesPreview();
